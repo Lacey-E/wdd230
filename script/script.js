c7b9942531030cc0800c1059b4187eee959d81a6
lacey-e <buchiezim@gmail.com>
2021-10-05 11:42:04 +0100

    dd

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const date = new Date();
+const finalday = (date.getFullYear());
+document.querySelector('#year').innerHTML = finalday
+
+
+const time = new Date();
+document.querySelector('#time').innerHTML = time
+
+  
