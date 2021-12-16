5a74087a65193cb828ac02a15c1b7d52ef62c523
lacey-e <buchiezim@gmail.com>
2021-12-16 22:45:27 +0100

    siueiuf

--- chamber of commerce/js/json.js
+++ chamber of commerce/js/json.js
@@ -18,7 +18,7 @@ function toggleMenu() {
 
 
 
-const requestURL = 'https://lacey-e.github.io/wdd230/chamber%20of%20commerce/business.json';
+const requestURL = 'https://lacey-e.github.io/wdd230/business.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -31,21 +31,20 @@ fetch(requestURL)
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
       let bday = document.createElement("p");
-      let bplace = document.createElement("p");
+     
       let image = document.createElement("img");
 
   
       card.setAttribute("class", `order${prophets[i].order}`)
       h2.textContent = `${prophets[i].name} ${prophets[i].lastname}`;
-      bday.textContent = `Birth Date: ${prophets[i].birthdate}`;
+      bday.textContent = `Birth Date: ${prophets[i].motto}`;
 
       
-      bplace.textContent = `Birth Place: ${prophets[i].birthplace}`;
+    
       image.setAttribute("src", prophets[i].imageurl);
       image.setAttribute("alt", `${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}`);
       card.appendChild(h2);
       card.appendChild(bday);
-      card.appendChild(bplace);
       card.appendChild(image);
 
       document.querySelector("div.cards").appendChild(card);
